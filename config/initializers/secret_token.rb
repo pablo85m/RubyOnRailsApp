393526857d4e9d862afde03408e45e00082f13d4
Pawel Marciniak <pablo@ubuntu.(none)>
2012-09-13 20:41:12 +0200

    Nowa aplikacja zostala stworzona.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyOnRailsApp::Application.config.secret_token = '7bf7b46f74da73438420966fa6ec83ec60f03fe0c29c4cfda93c475c89890bba843168b3fcd56511314bb5c933b338009c9e8043034d1b8bcc5d08c706a2d810'
